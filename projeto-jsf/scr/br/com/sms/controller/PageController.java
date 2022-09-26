2e80c6c0cd49ca57132a4fb35fe9f8c03be0b0ef
administrador <administrador@modelo3>
2022-09-26 09:39:42 -0300

    Update faces-config.pageflow, faces-config.xml, and 14 more files...

--- projeto-jsf/scr/br/com/sms/controller/PageController.java
+++ projeto-jsf/scr/br/com/sms/controller/PageController.java
@@ -1,15 +1,15 @@
-package br.com.sms.controller234234;
+package br.com.sms.controller;
 
 import java.io.Serializable;
 
-import javax.annotation.ManagedBean;
+import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
 @ManagedBean
 @SessionScoped
-public class PageController /* implements Serializable */ {
+public class PageController implements Serializable {
 	
-//	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
 	public String moveToPage1() {
 		System.out.println("Page 1");
